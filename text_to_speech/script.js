7532907ff7a23f52241dc1d17b0805da9b78ae81
MaliAnand01 <anandsundesha@gmail.com>
2025-06-06 17:09:50 +0530

    Improve JS code

--- text_to_speech/script.js
+++ text_to_speech/script.js
@@ -1,28 +1,40 @@
 const speech = new SpeechSynthesisUtterance();
-
 const select = document.querySelector('select');
 const btn = document.querySelector('button');
+const textArea = document.querySelector('#text-area');
 
-let voices = [];
-let voiceSelect = document.querySelector('select');
+let availableVoices = [];
 
-window.speechSynthesis.onvoiceschanged = () => {
-    voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
+function populateVoiceList() {
+    availableVoices = window.speechSynthesis.getVoices();
+    select.innerHTML = ''; // Clear existing options
+    availableVoices.forEach((voice, index) => {
+        const option = document.createElement('option');
+        option.textContent = voice.name;
+        option.value = index;
+        select.appendChild(option);
+    });
 
-    voices.forEach((voice, i) => ((voiceSelect.options[i]) = new Option(voice.name, i)));
-};
+    if (availableVoices.length > 0) {
+        speech.voice = availableVoices[0];
+    }
+}
 
-voiceSelect.addEventListener('change', ()=>{
-    speech.voice = voices[voiceSelect.value];
-})
+// Initial population and event listener for voices being loaded
+populateVoiceList();
+if (window.speechSynthesis.onvoiceschanged !== undefined) {
+    window.speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+select.addEventListener('change', () => {
+    speech.voice = availableVoices[select.value];
+});
 
 btn.addEventListener('click', () => {
-    speech.text = document.querySelector('#text-area').value;
+    speech.text = textArea.value;
     window.speechSynthesis.speak(speech);
 });
 
-
 select.addEventListener('click', () => {
     select.classList.toggle('open');
-})
+});
