d9cf8cff75e75ee551f64de6b3d1f642d1864f48
MaliAnand01 <anandsundesha@gmail.com>
2025-06-06 16:39:16 +0530

    Inital commit of text_to_speech project

---
+++ text_to_speech/script.js
@@ -0,0 +1,28 @@
+const speech = new SpeechSynthesisUtterance();
+
+const select = document.querySelector('select');
+const btn = document.querySelector('button');
+
+let voices = [];
+let voiceSelect = document.querySelector('select');
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => ((voiceSelect.options[i]) = new Option(voice.name, i)));
+};
+
+voiceSelect.addEventListener('change', ()=>{
+    speech.voice = voices[voiceSelect.value];
+})
+
+btn.addEventListener('click', () => {
+    speech.text = document.querySelector('#text-area').value;
+    window.speechSynthesis.speak(speech);
+});
+
+
+select.addEventListener('click', () => {
+    select.classList.toggle('open');
+})
